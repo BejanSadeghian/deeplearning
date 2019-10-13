6173c8e1cee7b50dd9cb790e9171d86f95577ad6
bejan <bsadeghian>
2019-10-12 19:37:05 -0500

    fcn_5blocks_3ctn_skip_res

--- homework3-remote-FCN/homework/models.py
+++ homework3-remote-FCN/homework/models.py
@@ -80,19 +80,28 @@ class FCN(torch.nn.Module):
 #        self.pooling = torch.nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
         self.init_padding = 1
         self.conv_1 = torch.nn.Conv2d(n_input_channels, 32, kernel_size=3, padding=self.init_padding, stride=1)
+        #Block 1
         self.conv_2 = torch.nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1)
-        self.conv_3 = torch.nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1)
+        self.conv_3 = torch.nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
+        #Block 2
         self.conv_4 = torch.nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1)
-        self.conv_5 = torch.nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1)
-        self.conv_6 = torch.nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        self.conv_7 = torch.nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
-        self.conv_8 = torch.nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
-        self.conv_9 = torch.nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+        self.conv_5 = torch.nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        #Block 3
+        self.conv_6 = torch.nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1)
+        self.conv_7 = torch.nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        #Block 4
+        self.conv_8 = torch.nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1)
+        self.conv_9 = torch.nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        #Block 5
+        self.conv_10 = torch.nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+        self.conv_11 = torch.nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
         
         self.convtr_1 = torch.nn.ConvTranspose2d(5, 5, kernel_size=3, stride=2, padding=1, output_padding=1)
         self.convtr_2 = torch.nn.ConvTranspose2d(5+64, 5, kernel_size=3, stride=2, padding=1, output_padding=1)
-        self.convtr_3 = torch.nn.ConvTranspose2d(5+32, 5, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.convtr_3 = torch.nn.ConvTranspose2d(5+64, 5, kernel_size=3, stride=2, padding=1, output_padding=1)
         self.convtr_4 = torch.nn.ConvTranspose2d(5+32, 5, kernel_size=3, stride=2, padding=1, output_padding=1)
+#        self.convtr_5 = torch.nn.ConvTranspose2d(5+32, 5, kernel_size=3, stride=2, padding=1, output_padding=1)
+#        self.convtr_6 = torch.nn.ConvTranspose2d(5+32, 5, kernel_size=3, stride=2, padding=1, output_padding=1)
         
         #No Skip connections
         self.convtr_1_ns = torch.nn.ConvTranspose2d(5, 5, kernel_size=3, stride=2, padding=1, output_padding=1)
@@ -100,35 +109,32 @@ class FCN(torch.nn.Module):
         self.convtr_3_ns = torch.nn.ConvTranspose2d(5, 5, kernel_size=3, stride=2, padding=1, output_padding=1)
         self.convtr_4_ns = torch.nn.ConvTranspose2d(5, 5, kernel_size=3, stride=2, padding=1, output_padding=1)
 
+        #Batch normalization
         self.bnorm_1 = torch.nn.BatchNorm2d(32)
         self.bnorm_2 = torch.nn.BatchNorm2d(32)
         self.bnorm_3 = torch.nn.BatchNorm2d(32)
         self.bnorm_4 = torch.nn.BatchNorm2d(64)
         self.bnorm_5 = torch.nn.BatchNorm2d(64)
-        self.bnorm_6 = torch.nn.BatchNorm2d(128)
-        self.bnorm_7 = torch.nn.BatchNorm2d(128)
-        self.bnorm_8 = torch.nn.BatchNorm2d(128)
+        self.bnorm_6 = torch.nn.BatchNorm2d(64)
+        self.bnorm_7 = torch.nn.BatchNorm2d(64)
+        self.bnorm_8 = torch.nn.BatchNorm2d(64)
         self.bnorm_9 = torch.nn.BatchNorm2d(128)
+        self.bnorm_10 = torch.nn.BatchNorm2d(128)
+        self.bnorm_11 = torch.nn.BatchNorm2d(128)
         
         self.bnormtr_1 = torch.nn.BatchNorm2d(5)
         self.bnormtr_2 = torch.nn.BatchNorm2d(5)
         self.bnormtr_3 = torch.nn.BatchNorm2d(5)
         self.bnormtr_4 = torch.nn.BatchNorm2d(5)
-        
-        self.mp_1 = torch.nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
-        self.mp_2 = torch.nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
-        self.mp_3 = torch.nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
 
-        self.res_1 = torch.nn.Conv2d(32, 32, kernel_size=1, stride=1) 
-        self.res_2 = torch.nn.Conv2d(32, 32, kernel_size=1, stride=2) 
-        self.res_3 = torch.nn.Conv2d(32, 32, kernel_size=1, stride=2) 
-        self.res_4 = torch.nn.Conv2d(32, 64, kernel_size=1, stride=2) 
-        self.res_5 = torch.nn.Conv2d(64, 64, kernel_size=1, stride=2) 
-        self.res_6 = torch.nn.Conv2d(64, 128, kernel_size=1, stride=1)
-        self.res_7 = torch.nn.Conv2d(128, 128, kernel_size=1, stride=1) 
-        self.res_8 = torch.nn.Conv2d(128, 128, kernel_size=1, stride=1) 
-        self.res_9 = torch.nn.Conv2d(128, 128, kernel_size=1, stride=1) 
-
+        #Residual connections
+        self.res_1 = torch.nn.Conv2d(32, 32, kernel_size=1, stride=2) 
+        self.res_2 = torch.nn.Conv2d(32, 64, kernel_size=1, stride=2) 
+        self.res_3 = torch.nn.Conv2d(64, 64, kernel_size=1, stride=2) 
+        self.res_4 = torch.nn.Conv2d(64, 128, kernel_size=1, stride=2) 
+#        self.res_5 = torch.nn.Conv2d(64, 64, kernel_size=1, stride=1) 
+ 
+        #Classifier
         self.classifier = torch.nn.Conv2d(128, 5, kernel_size=1)
         
 
@@ -143,62 +149,54 @@ class FCN(torch.nn.Module):
               convolution
         """
         z = x
-        z_1 = self.conv_1(z)
-        a_1 = self.relu(self.bnorm_1(z_1))
-#        a_1 = F.pad(a_1, (0,int(np.ceil(z.size(2)/2 % 1)), 0, int(np.ceil(z.size(3)/2 % 1))))
-            
-        z = a_1
-        z_2 = self.conv_2(z)
-        a_2 = self.relu(self.bnorm_2(z_2)) + self.res_2(z)
-#        a_2 = F.pad(a_2, (0,int(np.ceil(z.size(2)/2 % 1)), 0, int(np.ceil(z.size(3)/2 % 1))))
+        z_0 = self.conv_1(z)
+        a_0 = self.relu(self.bnorm_1(z_0))
         
-        z = a_2
-        z_3 = self.conv_3(z)
-        a_3 = self.relu(self.bnorm_3(z_3)) + self.res_3(z)
-        a_3 = self.mp_1(a_3)
-#        a_3 = F.pad(a_3, (0,int(np.ceil(z.size(2)/2 % 1)), 0, int(np.ceil(z.size(3)/2 % 1))))
+        #Block 1
+        z = a_0
+        z_1_1 = self.conv_2(z)
+        a_1_1 = self.relu(self.bnorm_2(z_1_1))
+        z_1_2 = self.conv_3(a_1_1)
+        a_1_2 = self.relu(self.bnorm_3(z_1_2)) + self.res_1(z)
         
-        z = a_3
-        z_4 = self.conv_4(z)
-        a_4 = self.relu(self.bnorm_4(z_4)) + self.res_4(z)
-#        a_4 = F.pad(a_4, (0,int(np.ceil(z.size(2)/2 % 1)), 0, int(np.ceil(z.size(3)/2 % 1))))
+        #Block 2
+        z = a_1_2
+        z_2_1 = self.conv_4(z)
+        a_2_1 = self.relu(self.bnorm_4(z_2_1))
+        z_2_2 = self.conv_5(a_2_1)
+        a_2_2 = self.relu(self.bnorm_5(z_2_2)) + self.res_2(z)
         
-        z = a_4
-        z_5 = self.conv_5(z)
-        a_5 = self.relu(self.bnorm_5(z_5)) + self.res_5(z)
-        a_5 = self.mp_2(a_5)
-#        a_5 = F.pad(a_5, (0,int(np.ceil(z.size(2)/2 % 1)), 0, int(np.ceil(z.size(3)/2 % 1))))
+        #Block 3
+        z = a_2_2
+        z_3_1 = self.conv_6(z)
+        a_3_1 = self.relu(self.bnorm_6(z_3_1))
+        z_3_2 = self.conv_7(a_3_1)
+        a_3_2 = self.relu(self.bnorm_7(z_3_2)) + self.res_3(z)
         
-        z = a_5
-        z_6 = self.conv_6(z)
-        a_6 = self.relu(self.bnorm_6(z_6)) + self.res_6(z)
-#        a_6 = F.pad(a_6, (0,int(np.ceil(z.size(2)/2 % 1)), 0, int(np.ceil(z.size(3)/2 % 1))))
+        #Block 4        
+        z = a_3_2
+        z_4_1 = self.conv_8(z)
+        a_4_1 = self.relu(self.bnorm_8(z_4_1))
+        z_4_2 = self.conv_9(a_4_1)
+        a_4_2 = self.relu(self.bnorm_9(z_4_2)) + self.res_4(z)
         
-        z = a_6
-        z_7 = self.conv_7(z)
-        a_7 = self.relu(self.bnorm_7(z_7)) + self.res_7(z)
-        a_7 = self.mp_3(a_7)
-#        a_7 = F.pad(a_7, (0,int(np.ceil(z.size(2)/2 % 1)), 0, int(np.ceil(z.size(3)/2 % 1))))
+        #Block 5        
+        z = a_4_2
+        z_5_1 = self.conv_10(z)
+        a_5_1 = self.relu(self.bnorm_10(z_5_1))
+        z_5_2 = self.conv_11(a_5_1)
+        a_5_2 = self.relu(self.bnorm_11(z_5_2)) + z #self.res_5(z) #No need for upsampling 
         
-        z = a_7
-        z_8 = self.conv_8(z)
-        a_8 = self.relu(self.bnorm_8(z_8)) + self.res_8(z)
-#        a_7 = F.pad(a_7, (0,int(np.ceil(z.size(2)/2 % 1)), 0, int(np.ceil(z.size(3)/2 % 1))))
-        
-        z = a_8
-        z_9 = self.conv_9(z)
-        a_9 = self.relu(self.bnorm_9(z_9)) + self.res_9(z)
-#        a_7 = F.pad(a_7, (0,int(np.ceil(z.size(2)/2 % 1)), 0, int(np.ceil(z.size(3)/2 % 1))))
-        
-        z = self.classifier(a_9)
+        z = self.classifier(a_5_2)
         
         z0 = self.bnormtr_1(self.convtr_1(z))
         try:
-            z = self.bnormtr_2(self.convtr_2(torch.cat((z0, a_4),1)))
-            z = self.bnormtr_3(self.convtr_3(torch.cat((z, a_3),1)))
-            z = self.bnormtr_4(self.convtr_4(torch.cat((z, a_2),1)))
-        except:
+            z = self.bnormtr_2(self.convtr_2(torch.cat((z0, a_3_2),1)))
+            z = self.bnormtr_3(self.convtr_3(torch.cat((z, a_2_2),1)))
+            z = self.bnormtr_4(self.convtr_4(torch.cat((z, a_1_2),1)))
+        except Exception as e:
             #no skip
+            print('no skip', e)
             z = self.bnormtr_2(self.convtr_2_ns(z0))
             z = self.bnormtr_3(self.convtr_3_ns(z))
             z = self.bnormtr_4(self.convtr_4_ns(z))
