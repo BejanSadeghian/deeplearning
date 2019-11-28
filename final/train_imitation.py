5deda791c387836e4966f84cfcd7dbd2fed3f2d2
bejan <bsadeghian>
2019-11-28 15:38:42 -0600

    removed pystk

--- final/train_imitation.py
+++ final/train_imitation.py
@@ -3,79 +3,17 @@ import torch.utils.tensorboard as tb
 import argparse
 import os
 import numpy as np
-import pystk
 from utils import load_data
 from agent_imitation.player import HockeyPlayer
 from agent_imitation.model import Action, save_model
 from agent_imitation.vision_model import Vision, load_vision_model
 import itertools
 from torch.distributions.normal import Normal
-# from utils import load_data
-
-class Player:
-    def __init__(self, player, team=0):
-        self.player = player
-        self.team = team
-
-    @property
-    def config(self):
-        return pystk.PlayerConfig(controller=pystk.PlayerConfig.Controller.PLAYER_CONTROL, kart=self.player.kart, team=self.team)
-    
-    def __call__(self, image, player_info):
-        return self.player.act(image, player_info)
 
 def getRMSE(list_preds, list_targets, idx):
     predicted = np.array([x[idx] for x in list_preds])
     targets = np.array([x[idx] for x in list_targets])
     return np.sqrt(((predicted - targets)**2).mean())
-
-
-def rollout_agent(device, vision, action, n_steps=200):
-    race_config = pystk.RaceConfig(num_kart=1, track='icy_soccer_field', mode=pystk.RaceConfig.RaceMode.SOCCER)
-
-    k = pystk.Race(race_config)
-    k.start()
-    for i in range(5): #Skip the first 5 steps since its the game starting
-        k.step() 
-    try:
-        data = []
-        for n in range(n_steps):
-            img = torch.tensor(np.array(k.render_data[0].image), dtype=torch.float).to(device).permute(2,0,1)
-            # heatmap = vision(img)
-            # p = action(torch.sigmoid(heatmap))[0]
-            p = action(img)[0]
-            # print(p[0])
-            k.step(pystk.Action(steer=float(p[0]), acceleration=float(p[1]), brake=float(p[2])>0.5)) #TODO: remove /10 later
-            # print(pystk.Action(acceleration=float(p[0]), steer=float(p[1]), brake=float(p[2])>0.5))
-            la = k.last_action[0]
-            # print((la.acceleration, la.steer, la.brake))
-            # print('end')
-            data.append((np.array(k.render_data[0].image), (la.steer, la.acceleration, la.brake))) #TODO: remove /10 later
-    finally:
-        k.stop()
-        del k
-    return data
-
-def rollout(device, vision, action, n_steps=200):
-    race_config = pystk.RaceConfig(num_kart=1, track='icy_soccer_field', mode=pystk.RaceConfig.RaceMode.SOCCER)
-    o = pystk.PlayerConfig(controller = pystk.PlayerConfig.Controller.AI_CONTROL, team = 0)
-    race_config.players.append(o)
-    k = pystk.Race(race_config)
-    
-    k.start()
-    for i in range(5): #Skip the first 5 steps since its the game starting
-        k.step() 
-    try:
-        data = []
-        for n in range(n_steps):
-            k.step()
-            la = k.last_action[0]
-
-            data.append((np.array(k.render_data[0].image), (la.steer, la.acceleration, la.brake))) #TODO: remove /10 later 3 in this script and 1 in player
-    finally:
-        k.stop()
-        del k
-    return data
 
 def train(args):
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
@@ -99,14 +37,7 @@ def train(args):
 
     if args.logdir is not None:
         train_logger = tb.SummaryWriter(log_dir=os.path.join(args.logdir, 'train_{}'.format(args.log_suffix)), flush_secs=1)  
-    
-    #PySTK init
-    config = pystk.GraphicsConfig.hd()
-    config.screen_width = 400
-    config.screen_height = 300
-    pystk.init(config)
 
-    # train_data = list(itertools.chain(*[rollout(device, vision_model, model) for it in range(10)]))
     global_step = 0
     for e in range(args.epochs):
         all_targets = []
@@ -119,13 +50,10 @@ def train(args):
             x2 = labels[:,:,3:].squeeze(1)
             labels = labels[:,:,:3].squeeze(1)
             
-            # print(images.shape)
             heatmaps, reshaped_images = vision_model(images)
-            # print(heatmaps)
             combined_images = torch.cat((reshaped_images, torch.sigmoid(heatmaps)), 1)
-            # print('shapes')
-            # print(combined_images.shape)
-            # print(combined_images.shape, images.shape, heatmaps.shape)
+
+
             pred = model(combined_images, x2)
             l = loss(pred, labels.squeeze())
 
